fae74fbe5729406c49cd1e666fb24564f3e20f2c
vezoffy <ritesh060702@gmail.com>
2026-05-29 01:23:52 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for(String s : strs){
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = new String(charArray);
+            res.putIfAbsent(sortedS, new ArrayList<>());
+            res.get(sortedS).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
+
+
+// Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+// Example 1:
+
+// Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+// Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+// Example 2:
+
+// Input: strs = ["x"]
+
+// Output: [["x"]]
+// Example 3:
+
+// Input: strs = [""]
+
+// Output: [[""]]
+// Constraints:
+
+// 1 <= strs.length <= 1000.
+// 0 <= strs[i].length <= 100
+// strs[i] is made up of lowercase English letters.
