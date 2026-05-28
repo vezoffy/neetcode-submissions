e806ed9f154ae72bee70f5a0ddccf1d41a96e9b1
vezoffy <ritesh060702@gmail.com>
2026-05-29 00:50:37 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,27 @@
+// Two Sum
+
+// Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+// You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+// Return the answer with the smaller index first.
+
+// Example 1:
+
+// Input: 
+// nums = [3,4,5,6], target = 7
+
+// Output: [0,1]
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j  = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
